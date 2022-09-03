3635cd74c71e5e7af5fc697bbd4bd91cd640d685
오하늘 <nebulass@naver.com>
2022-09-04 06:38:46 +0900

    router 수정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './routes/Home';
 import Detail from './routes/Detail';
 import './style/scss/style.scss';
