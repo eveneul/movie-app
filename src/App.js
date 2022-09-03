be6813e031fe38c8167d4e026f54352942eb4c24
오하늘 <nebulass@naver.com>
2022-09-04 06:44:43 +0900

    배포 수정..

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './routes/Home';
 import Detail from './routes/Detail';
 import './style/scss/style.scss';
